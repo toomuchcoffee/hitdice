9a01ab572181fe7f9c953901de27c864eb7baa8e
toomuchcoffee <2muchcoffee@gmx.de>
2020-05-07 23:35:59 +0200

    bugfixes

--- src/main/java/de/toomuchcoffee/hitdice/controller/GameController.java
+++ src/main/java/de/toomuchcoffee/hitdice/controller/GameController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -32,7 +33,7 @@ public class GameController {
     public String loadGame(@PathVariable Integer id, HttpSession session) {
         Hero hero = gameService.restore(id);
         session.setAttribute("hero", hero);
-        return "game/mode";
+        return "redirect:/game/mode";
     }
 
     @GetMapping("delete/{id}")
@@ -42,9 +43,15 @@ public class GameController {
     }
 
     @GetMapping("save")
-    public String saveGame(@SessionAttribute Hero hero) {
+    public String saveGame(@SessionAttribute Hero hero, WebRequest request) {
         gameService.save(hero);
-        return "redirect:/dungeon";
+        String referer = request.getHeader("Referer");
+        return "redirect:" + referer;
+    }
+
+    @GetMapping("mode")
+    public String chooseMode() {
+        return "game/mode";
     }
 
     @GetMapping("mode/{mode}")
--- src/main/java/de/toomuchcoffee/hitdice/controller/HomeController.java
+++ src/main/java/de/toomuchcoffee/hitdice/controller/HomeController.java
@@ -3,12 +3,17 @@ package de.toomuchcoffee.hitdice.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("*")
 public class HomeController {
     @GetMapping
-    public String home() {
+    public String home(HttpSession session, SessionStatus status) {
+        status.setComplete();
+        session.invalidate();
         return "home";
     }
 }
